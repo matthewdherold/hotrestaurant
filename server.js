93b3c713988aab1a881df9f37717a52b356c6e21
Matt H <matthewdherold@gmail.com>
2021-03-04 20:24:50 -0800

    added more functionality to server.js

--- server.js
+++ server.js
@@ -6,23 +6,9 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const tables = [
-    {
-        name: 'Mandy',
-        phoneNumber: '(123)-456-7890',
-        email: 'mandylikestreats@gmail.dog',
-        uniqueID: '999111333',
-    },
-];
+const tables = [];
 
-const waitList = [
-    {
-        name: 'Mandy',
-        phoneNumber: '(123)-456-7890',
-        email: 'mandylikestreats@gmail.dog',
-        uniqueID: '999111333',
-    },
-];
+const waitList = [];
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
@@ -34,24 +20,34 @@ app.get('/api/tables', (req, res) => res.json(tables));
 
 app.get('/api/waitList', (req, res) => res.json(waitList));
 
+app.get('/api/tables/available', (req, res) => res.json(5 - tables.length));
+
 app.post('/api/tables', (req, res) => {
   const newTable = req.body;
-
   console.log(newTable);
-
-  tables.push(newTable);
-
-  res.json(newTable);
+    if (tables.length >= 5) {
+        waitList.push(newTable);
+        console.log('added to waitlist');
+        res.json(false);
+    } else {
+        tables.push(newTable);
+        console.log('added to reservations');
+        res.json(true);
+    };
 });
 
-app.post('/api/waitList', (req, res) => {
-    const newEntry = req.body;
-  
-    console.log(newEntry);
-  
-    waitList.push(newEntry);
-  
-    res.json(newEntry);
-  });
+app.post('/api/clear', (req, res) => {
+    tables.length = 0;
+    waitList.length = 0;
+    res.json('cleared');
+});
+
+app.get('/api/log', (req, res) => {
+    console.log('tables:');
+    console.log(tables);
+    console.log('waitlist:');
+    console.log(waitList);
+    res.json('logged');
+});
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
