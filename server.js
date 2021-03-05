78393592361863d0a16d4a83894d9a4334d2d886
Matt H <matthewdherold@gmail.com>
2021-03-04 19:11:46 -0800

    adding server.js

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const tables = [
+    {
+        name: 'Mandy',
+        phoneNumber: '(123)-456-7890',
+        email: 'mandylikestreats@gmail.dog',
+        uniqueID: '999111333',
+    },
+];
+
+const waitList = [
+    {
+        name: 'Mandy',
+        phoneNumber: '(123)-456-7890',
+        email: 'mandylikestreats@gmail.dog',
+        uniqueID: '999111333',
+    },
+];
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+
+app.post('/api/tables', (req, res) => {
+  const newTable = req.body;
+
+  console.log(newTable);
+
+  tables.push(newTable);
+
+  res.json(newTable);
+});
+
+app.post('/api/waitList', (req, res) => {
+    const newEntry = req.body;
+  
+    console.log(newEntry);
+  
+    waitList.push(newEntry);
+  
+    res.json(newEntry);
+  });
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
